a91a36750780dddf668ff2ddd7d9d24858353ea6
franzhanl <franz.hanl@hotmail.com>
2023-10-17 17:49:30 -0300

    New content in 'comer intuitivo'

--- src/js/script.js
+++ src/js/script.js
@@ -2,23 +2,25 @@
 const item2 = document.getElementById('item-2')
 const item2Image = item2.querySelector('img')
 const item2P = item2.querySelector('p')
-
 const itemContent2 = document.getElementById('item-content-2')
+
+const item3 = document.getElementById('item-3')
+const item3Image = item3.querySelector('img')
+const item3P = item3.querySelector('p')
+const itemContent3 = document.getElementById('item-content-3')
 
 item2.onclick = () => {
     item2.classList.toggle('item-clicked')
-    itemContent2.classList.toggle('item-content-opened')
     item2Image.classList.toggle('item-content-closed')
     item2P.classList.toggle('item-content-closed')
 
-    // if(itemContent2.classList == 'item-content-closed'){
-    //     itemContent2.classList.remove('item-content-closed')
-    //     itemContent2.classList.add('item-content-opened')  
-    // }else{
-    //     itemContent2.classList.remove('item-content-opened')
-    //     itemContent2.classList.add('item-content-closed')
-    // }
-    
+    itemContent2.classList.toggle('item-content-opened')
 } 
 
-console.log(item2)
+item3.onclick = () => {
+    item3.classList.toggle('item-clicked')
+    item3Image.classList.toggle('item-content-closed')
+    item3P.classList.toggle('item-content-closed')
+
+    itemContent3.classList.toggle('item-content-opened')
+} 
